0162a990ec382119c906545b313ee7996a92c7f5
admin <bj1570saber@gmail.com>
2019-12-12 18:38:30 -0800

    view_func

--- fisher.py
+++ fisher.py
@@ -6,9 +6,14 @@ app = Flask(__name__) # default project name
 # A file has multiple url will be mark as cheating search engine.
 # That's why unique url is important. Flask will redirect the correct url
 def hello(): # simple view function: http://127.0.0.1:5000/hello
-
     return 'Hello, visitor. with debug mode'
 
+# another way to direct a page: app.add_url_rule()
+def greeting():
+    return 'Welcome to greeting page.'
+
+app.add_url_rule('/greeting', view_func=greeting)
+# http://127.0.0.1:5000/greeting
 
 app.run(debug = True)
 
