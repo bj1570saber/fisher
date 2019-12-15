919690da1ab84b388247aa53d15bf98887386dc3
admin <bj1570saber@gmail.com>
2019-12-14 16:00:56 -0800

    finish

--- fisher.py
+++ fisher.py
@@ -1,17 +1,46 @@
-from flask import Flask
+from flask import Flask, make_response
 from config import DEBUG
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    return '<html><body><h1>love</h1></body></html>'
+    # 404:
+    headers = {
+        'content-type': 'text/plain'
+    }
+    response = make_response('<html><body><h1>love</h1></body></html>', 404)
+    # 404: Not found page, but will display anyway.
+
+    # 301:
+    '''
+    headers = {
+        'content-type': 'text/plain',
+        'location': 'http://www.bing.com'
+    }
+    
+    response = make_response('<html><body><h1>love</h1></body></html>', 301)
+    # 301:redirect to specific 'location' in headers. Content body will not display.
+    # Another way to return:
+    #return '<html></html>', 301, headers
+    '''
+    # return app api:
+    '''
+    headers = {
+        'content-type': 'application/json',
+        'location': 'http://www.bing.com'
+    }
+    '''    '''
+    response = make_response('<html><body><h1>love</h1></body></html>', 404)
+    '''
+    # response.headers = headers
+    # return response
 '''
  also return other info:
  status code 200, 404, 301...
  content-type http headers, default: text/html
- 
  '''
+
 
 def greeting():
     return 'Welcome to greeting page.'
