90b670c435f4d9963f6fbd3914c6b3d7f897e968
admin <bj1570saber@gmail.com>
2019-12-29 23:38:39 -0800

    WTForms validate request parameters

--- app/web/book.py
+++ app/web/book.py
@@ -1,3 +1,5 @@
+from ..forms.book import SearchForm
+
 print(__name__)
 
 from flask import jsonify, request
@@ -5,9 +7,10 @@ from flask import jsonify, request
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
 from . import web
+
+
 # print('book_1: ', id(app)) # for debug
 # blueprint -> resolve cyclical import
-
 
 
 @web.route('/book/search')  # http://0.0.0.0:81/book/search?q=9787501524044&page=1
@@ -16,13 +19,20 @@ def search():
     q(isbn -> q): keyword || isbn ;
     page (start, count):
     """
-    q = request.args['q'] # request must be called by 'view functions' or it's sub functions;
-    page = request.args['page']
 
-    isbn_or_key = is_isbn_or_key(q)  # return 'key' or 'isbn'
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)  # return dict type
+    form = SearchForm(request.args)  # request must be called by 'view functions' or it's sub functions;
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data  # form may assign a default value, so it's better than: page = request.args['page']
+
+        isbn_or_key = is_isbn_or_key(q)  # return 'key' or 'isbn'
+        if isbn_or_key == 'isbn':
+            result = YuShuBook.search_by_isbn(q)  # return dict type
+        else:
+            result = YuShuBook.search_by_keyword(q)  # return dict type
+        #  return json.dumps(result), 200, {'content-type':'application/json'}
+        return jsonify(result)  # flask-jsonify()
     else:
-        result = YuShuBook.search_by_keyword(q)  # return dict type
-    #  return json.dumps(result), 200, {'content-type':'application/json'}
-    return jsonify(result)  # flask-jsonify()
+        return jsonify({'msg':'Invalid parameters'})
+
+
